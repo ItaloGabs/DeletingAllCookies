d882041968066f2e5d34e6bb3a6c26e445924068
Ítalo Gabriel <gabrielgg.tavares@gmail.com>
2022-08-24 07:57:07 -0300

    1.0.1
    Comentarios adicionados

--- deleting.js
+++ deleting.js
@@ -3,16 +3,17 @@ const deletandoCookie = (nome, path, domain) => {
       ((path) ? ";path=" + path : "") +
       ((domain) ? ";domain=" + domain : "") +
       ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+      //insere uma data para expirar o cookie e ser removido
       console.log('excluiu ' + nome)
 }
 
 function listaCookies(){
-    var cookies = document.cookie.split(";");
+    var cookies = document.cookie.split(";"); //Separa todos os cookies um do outro
     for (var i = 0; i < cookies.length; i++) {
         var cookie = cookies[i];
         var Apos = cookie.indexOf("=");
         var name = Apos > -1 ? cookie.substring(0, Apos) : cookie;
-        deletandoCookie(name,'pathSite','DomainSite')
+        deletandoCookie(name,'path','Domain') //Path - path do site do seu dominio, Domain - dominio do site surgerido
         
     }
 }
